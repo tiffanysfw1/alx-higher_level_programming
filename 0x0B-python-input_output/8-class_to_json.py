c126e0da5e6b443d94d1e3ec5b3e732c263f7fd2
coder <122115043+tiffanysfw1@users.noreply.github.com>
2023-04-12 21:10:50 +0300

    Create 8-class_to_json.py

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+# tiffany chetalam
+
+"""defines a function that returns the dictionary
+    description
+"""
+
+
+def class_to_json(obj):
+    """returns a dictionary decription of a simple data structure"""
+    return obj.__dict__
